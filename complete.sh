5e5047c42ef641c23982f24aed10d98c41d0df45
Edward Suarez <contact@initr5.com>
2021-08-30 03:21:26 -0500

    Update some changes

--- complete.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-cd ~
-cd ~/.config/bspwm
-wget https://raw.githubusercontent.com/kyb3rvizsla-others/bspwm/master/examples/bspwmrc
-chmod +x bspwmrc
-cd ~/.config/sxhkd
-wget https://raw.githubusercontent.com/kyb3rvizsla-others/bspwm/master/examples/sxhkdrc
-chmod +x sxhkrc
-cd ~
--- install.sh
+++ install.sh
@@ -36,104 +36,26 @@ rm ~/google-chrome-stable_current_amd64.deb
 sudo apt-get update
 echo -e "\n${morado}[Chrome Browser instalado]${endColour}\n"
 
-echo -e "\n${amarillo}[Instalando y configurando bspwm y sxhkd]${endColour}\n"
-# Instalación de bspwm y sxhkd
-sudo apt-get install bspwm libxcb-xinerama0-dev libxcb-icccm4-dev libxcb-randr0-dev libxcb-util0-dev libxcb-ewmh-dev libxcb-keysyms1-dev libxcb-shape0-dev -y
-cd ~
-git clone https://github.com/kyb3rvizsla-others/bspwm.git
-git clone https://github.com/baskerville/sxhkd.gita
-cd bspwm && make && sudo make install
-cd ../sxhkd && make && sudo make install
-mkdir -p ~/.config/{bspwm,sxhkd}
-cp /usr/local/share/doc/bspwm/examples/bspwmrc ~/.config/bspwm/
-cp /usr/local/share/doc/bspwm/examples/sxhkdrc ~/.config/sxhkd/
-chmod u+x ~/.config/bspwm/bspwmrc
-echo -e "\n${morado}[bspwm y sxhkd instaldos y configurados]${endColour}\n"
-
-echo -e "\n${amarillo}[Creando y configurando el archivo ~/.xinitrc]${endColour}\n"
-# Creación y configuración del archivo ~/.xinitrc 
-cd ~
-wget https://raw.githubusercontent.com/kyb3rvizsla-others/configs_Kyb3rvotarOS/master/themes/.xinitrc
-cd ~
-echo -e "\n${morado}[.xinitrc fue creado y configurado]${endColour}\n"
-
-# Configuración de compton
-echo -e "\n${amarillo}[Creando y configurando el compton.conf]${endColour}\n"
-cd ~/.config/
-mkdir compton
-cd ~/.config/compton/
-wget https://raw.githubusercontent.com/kyb3rvizsla-others/configs_Kyb3rvotarOS/master/themes/compton.conf
-echo -e "\n${morado}[compton.conf fue creado y configurado]${endColour}\n"
-
-echo -e "\n${amarillo}[Configurando el wallpaper]${endColour}\n"
-# Configurando el wallpaper
-cd ~
-mkdir .kyb3rvotaros
-cd .kyb3rvotaros
-wget https://raw.githubusercontent.com/kyb3rvizsla-others/configs_Kyb3rvotarOS/master/themes/newwallpaper.jpg
-echo -e "\n${morado}[wallpaper configurado]${endColour}\n"
-
-echo -e "\n${azul}[BSPWM y SXHKD han sido configurados con exito]${endColour}\n"
-
-echo -e "\n${amarillo}[Creando el lanzador de la polybar]${endColour}\n"
-# Creando el lanzador de la polybar
-cd ~
-cd .config
-mkdir polybar
-cd polybar
-wget https://raw.githubusercontent.com/kyb3rvizsla-others/configs_Kyb3rvotarOS/master/themes/launch.sh
-chmod +x launch.sh
-echo -e "\n${morado}[Lanzador configurado]${endColour}\n"
-
 echo -e "\n${amarillo}[Instalando Hack Nerd Fonts]${endColour}\n"
 # Instalación de Hack Nerd Fonts
 cd /usr/local/share/fonts/
-sudo wget https://github.com/kyb3rvizsla-others/configs_Kyb3rvotarOS/raw/master/themes/Hack.zip
+sudo wget https://github.com/initr5/Kyb3rvotarOS/blob/master/src/themes/Hack.zip?raw=true
 sudo unzip Hack.zip
 sudo rm Hack.zip
 echo -e "\n${morado}[Hack Nerd Fonts instaldo]${endColour}\n"
 
-echo -e "\n${amarillo}[Instalando la polybar]${endColour}\n"
+echo -e "\n${amarillo}[Instalando dependencias]${endColour}\n"
 # Instalando la polybar
 sudo apt-get install build-essential git cmake cmake-data pkg-config python3-sphinx libcairo2-dev libxcb1-dev libxcb-util0-dev libxcb-randr0-dev libxcb-composite0-dev python3-xcbgen xcb-proto libxcb-image0-dev libxcb-ewmh-dev libxcb-icccm4-dev -y
 sudo apt-get install libxcb-xkb-dev libxcb-xrm-dev libxcb-cursor-dev libasound2-dev libpulse-dev i3-wm libjsoncpp-dev libmpdclient-dev libcurl4-openssl-dev libnl-genl-3-dev -y
-cd /opt
-sudo wget https://github.com/jaagr/polybar/releases/download/3.4.0/polybar-3.4.0.tar
-sudo tar -xf polybar-3.4.0.tar
-sudo rm /opt/polybar-3.4.0.tar
-sudo cd /opt/polybar/
-sudo mkdir /opt/polybar/build/
-cd /opt/polybar/build/
-sudo cmake ..
-sudo make -j$(nproc)
-sudo make install
-echo -e "\n${morado}[Polybar instalado]${endColour}\n"
-
-
-echo -e "\n${amarillo}[Creando y configurando modulos de la polybar]${endColour}\n"
-# Configurando modulos de la polybar
-cd ~/.config/
-mkdir bin
-cd ~/.config/bin/
-wget https://raw.githubusercontent.com/kyb3rvizsla-others/configs_Kyb3rvotarOS/master/bin/ethernet_status.sh
-chmod +x ~/.config/bin/ethernet_status.sh
-wget https://raw.githubusercontent.com/kyb3rvizsla-others/configs_Kyb3rvotarOS/master/bin/vpn_status.sh
-chmod +x ~/.config/bin/vpn_status.sh
-echo -e "\n${morado}[Modulos creados y configurados]${endColour}\n"
-
-# Configurando polybar
-cd ~/.config/polybar
-wget https://raw.githubusercontent.com/kyb3rvizsla-others/configs_Kyb3rvotarOS/master/themes/config
-mv config ~/.config/polybar/config
-
-echo -e "\n${azul}[La polybar ha sido configurada con exito]${endColour}\n"
+echo -e "\n${morado}[Dependencias instaladas.]${endColour}\n"
 
 # Intalando el zsh 
 sudo apt-get install zsh -y
 rm .zshrc
 cd ~
 wget https://raw.githubusercontent.com/kyb3rvizsla-others/Kyb3rvotarOS/master/config/zsh/zshrc
-mv zshrc ~/.zshrc
+
 
 
 echo -e "\n${amarillo}[Descargando y confurando los functions]${endColour}\n"
@@ -186,7 +108,7 @@ echo -e "\n${morado}[i3lock-fancy i image instaldos]${endColour}\n"
 
 echo -e "\n${rojo}[Corrigiendo errores en la instalación...]${endColour}\n"
 cd ~
-wget https://raw.githubusercontent.com/kyb3rvizsla-others/Kyb3rvotarOS/master/complete.sh
+wget https://raw.githubusercontent.com/initr5/Kyb3rvotarOS/master/complete.sh
 chmod +x complete.sh
 ./complete.sh
 echo -e "\n${morado}[Errores corregidos]${endColour}\n"
